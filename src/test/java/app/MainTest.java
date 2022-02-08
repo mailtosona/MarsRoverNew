7af0229bbd3d0bf0f76844d00523a140aa9be1d4
mailtosona <mailtosona@ymail.com>
2022-02-08 13:29:52 +0000

    addede more TDD

--- src/test/java/app/MainTest.java
+++ src/test/java/app/MainTest.java
@@ -21,5 +21,45 @@ class MainTest {
 
         assertEquals("Rover -1 1 3 N on Plateau-1",Main.start(capcom,in,args));
     }
+    @Test
+    public void shouldTakeFileAsInputToStartMethod() {
+        NASACapcomService capcom = new NASACapcomService();
+        String[] args = {"testdata\\shouldTakeFileInputTest.txt"};
+        InputStream in = System.in;
+        assertEquals("ROVER-1 at 1 3 N on PLATEAU-1",  Main.start(capcom, in,args));
+    }
+
+    @Test
+    public void shouldStopProcessingIfFileNotFound() {
+        NASACapcomService capcom = new NASACapcomService();
+        String[] args = {"nosuchfile.nosuchfile"};
+        InputStream in = System.in;
+        assertNull(Main.start(capcom, in,args));
+    }
+
+    @Test
+    public void shouldHandleInvalidCommandInFile() {
+        NASACapcomService capcom = new NASACapcomService();
+        String[] args = {"testdata\\shouldHandleInvalidCommandInFile.txt"};
+        InputStream in = System.in;
+        assertEquals("ROVER-1 at 1 3 N on PLATEAU-1",  Main.start(capcom,in,args));
+    }
+
+    @Test
+    public void shouldTestMapShowAndHide() {
+        NASACapcomService capcom = new NASACapcomService();
+        String[] args = {"testdata\\quadMap.txt"};
+        InputStream in = System.in;
+        assertEquals("ROVER-1 at 0 7 W on PLATEAU-1\nROVER-2 at 500 493 S on PLATEAU-2",  Main.start(capcom,in,args));
+    }
+
+    @Test
+    public void shouldHandleBigMoves() {
+        NASACapcomService capcom = new NASACapcomService();
+        String[] args = {"testdata\\bigMap.txt"};
+        InputStream in = System.in;
+        assertEquals("ROVER-1 at 0 0 N on PLATEAU-1",  Main.start(capcom,in,args));
+    }
+
 
 }
